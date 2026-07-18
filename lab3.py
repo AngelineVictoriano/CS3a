032ea0fda69f0b61ac922ceb2268d9c42414fb6d
AngelineVictoriano <114611719+AngelineVictoriano@users.noreply.github.com>
2026-07-18 01:02:50 -0700

    1st

---
+++ lab3.py
@@ -0,0 +1,18 @@
+def ticket_sales_income(atix, btix, ctix):
+    income = 20*atix+ 15*btix + 10*ctix
+    return income
+
+def main():
+    try:
+        aSold = int(input("How many class A tickets were sold? "))
+        bSold = int(input("How many class B tickets were sold? "))
+        cSold = int(input("How many class C tickets were sold? "))
+        total = ticket_sales_income(aSold, bSold, cSold)
+
+        print(f"The total sales generated is ${total}")
+    except ValueError:
+        print("Enter only integers!")
+        main()
+
+
+main()
